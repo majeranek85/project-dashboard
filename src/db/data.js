68f555c2abdfb228da098fed71924a6a108e2e04
Marta Majerowska <majeranek85@gmail.com>
2020-02-03 22:50:18 +0100

    Add db

---
+++ src/db/data.js
@@ -0,0 +1,52 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
+const dataSource = {};
+
+dataSource.links = {
+  home: {
+    title: 'Home page',
+    url: 'http://www.homepage.com',
+  },
+
+  currantly: {
+    title: 'Currantly offer',
+    url: 'http://www.currantly.com',
+  },
+
+  blurpix: {
+    title: 'Blurpix',
+    url: 'http://www.blurpix.com',
+  },
+
+  site: {
+    title: 'Your site',
+    url: 'http://www.yoursite.com',
+  },
+
+  google: {
+    title: 'Google',
+    url: 'http://www.google.com',
+  },
+
+  facebook: {
+    title: 'Facebook',
+    url: 'http://www.facebook.com',
+  },
+
+  dashboard: {
+    title: 'Dashboard',
+    url: 'http://www.dashboard.com',
+  },
+
+  blurax: {
+    title: 'Blurax',
+    url: 'http://www.blurax.com',
+  },
+
+  twitter: {
+    title: 'Twitter',
+    url: 'http://www.twitter.com',
+  },
+};
