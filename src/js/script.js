147fe384b729f6243d3a4717f96a2fb00730d3f7
Marta Majerowska <majeranek85@gmail.com>
2020-02-03 17:12:53 +0100

    Add mobile menu activation

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
+/* Mobile navigation */
+const mobileMenu = document.querySelector('.navigation');
+
+console.log(mobileMenu);
+
+const hamburger = document.getElementById('mobile-menu');
+console.log(hamburger);
+
+hamburger.addEventListener('click', function(event){
+  event.preventDefault();
+  mobileMenu.classList.toggle('active');
+});
+
+/* Page activation */
